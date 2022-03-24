4200b8414c347571e66f8bf4078aa91d3937b81d
Ântony Salvi <nysalvi20@gmail.com>
2022-03-24 17:29:44 -0300

    .

--- PeriodosAtras.ConsoleApp/Calendario.cs
+++ PeriodosAtras.ConsoleApp/Calendario.cs
@@ -9,29 +9,31 @@ namespace PeriodosAtras.ConsoleApp
     internal class Calendario
     {
         DateTime? data2, data1;
-        int dia, mes, ano;        
+        int dia, semana, mes, ano;        
         TimeSpan? intervalo;
-        public DateTime?[] ObterData()
+        public bool ObterData()
         {
             Console.WriteLine("Digite a data inicial : ");
             string primeiraData = Console.ReadLine();
             Console.WriteLine("Digite a data final : ");
             string segundaData = Console.ReadLine();
 
-            DateTime.TryParse(primeiraData, out DateTime data1);
-            DateTime.TryParse(segundaData, out DateTime data2);
+            DateTime? data1 = DateTime.Parse(primeiraData);
+            DateTime? data2 = DateTime.Parse(primeiraData);
 
             bool sucesso = ValidaData(data1, data2);
             if (sucesso)
             {
                 Console.WriteLine("As duas Datas são válidas !");
-                return new DateTime?[] { this.data1, this.data2 };
+                this.data1 = data1;
+                this.data2 = data2;
+                return true;
             }
             else
                 Console.WriteLine("Uma ou Ambas as Datas são Inválidas !!!");
-            return null;
+            return false;
         }
-        public bool ValidaData(DateTime? primeiraData, DateTime? segundaData)
+        private bool ValidaData(DateTime? primeiraData, DateTime? segundaData)
         {
             bool primeiraDataInvalida = primeiraData == null || primeiraData < DateTime.MinValue 
                 || primeiraData > DateTime.Now;
@@ -56,10 +58,21 @@ namespace PeriodosAtras.ConsoleApp
             intervalo = segundaData - primeiraData;
             return true;
         }
-        public void CalculaTempo()
+        public int[] CalculaTempo()
         {
             if (data1 == null || data2 == null)
-                return;
+                return null;
+            ano = data2.Value.Year - data1.Value.Year;
+            if (data2.Value.Year - data1.Value.Year < 0)
+                ano *= -1;
+            mes = data2.Value.Month - data1.Value.Month;
+            if (data2.Value.Month - data1.Value.Month < 0)
+                mes *= -1;
+            semana = (data2.Value.Day - data1.Value.Day) / 7;
+            dia = (data2.Value.Day - data1.Value.Day) - semana * 7;
+            if (data2.Value.Day - data1.Value.Day < 0)
+                dia *= -1;
+            return new int[4] { dia, semana, mes, ano };
         }
     }
 }
--- PeriodosAtras.ConsoleApp/Program.cs
+++ PeriodosAtras.ConsoleApp/Program.cs
@@ -4,10 +4,14 @@ namespace PeriodosAtras.ConsoleApp
 {
     internal class Program
     {
+        enum PeriodoTempo { dias, semanas, meses, anos};
         static void Main(string[] args)
-        {
+        {            
             Calendario calendario = new Calendario();
             string opcao;
+            int[] periodo;
+            (int dias,int semanas) = ((int) PeriodoTempo.dias, (int) PeriodoTempo.semanas);
+            (int meses,int anos) = ((int)PeriodoTempo.meses, (int) PeriodoTempo.anos);
             while (true)
             {                
                 Console.WriteLine("Digite 1 - para escolher");
@@ -17,10 +21,18 @@ namespace PeriodosAtras.ConsoleApp
                     break;
                 if (opcao == "1")
                 {
-                    calendario.ObterData();
-                    calendario.CalculaTempo();
-                }
+                    bool valido = calendario.ObterData();
+                    periodo = calendario.CalculaTempo();
+                    bool periodoValido = periodo != null;
+                    if (periodoValido)
+                    {
+                        Console.WriteLine("Anos : {0}\nMeses : {1}\nSemanas : {2}\nDias : {3}",
+                            periodo[anos], periodo[meses], periodo[semanas], periodo[dias]);
+                    }
                     
+                }
+                Console.ReadLine();
+                Console.Clear();
             }
         }
     }
